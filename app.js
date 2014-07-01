aefe378ca0804cec71f0badbf662c675eeb1f69b
john <john.cfmr.2009@gmail.com>
2014-07-01 15:55:46 -0500

    Fixed server running

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+#!/usr/bin/env node
+
+var debug = require('debug')('myapp');
+
+
+
 var express = require('express');
 
 
@@ -11,6 +17,32 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.set('port', process.env.PORT || 3000);
+
+//var server = app.listen(app.get('port'), function() {
+//  debug('Express server listening on port ' + server.address().port);
+//});
+
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendfile('index.html');
+});
+
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+    console.log(543);
+    socket.on('event_mouse', function(data){
+        console.log('message: ' + data);
+        io.emit('msg', data);
+    });
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -57,8 +89,6 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
-module.exports = app;
 
 
 //var io = require('socket.io').listen(app);
